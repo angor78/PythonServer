f333a47563820fcd7f3a470773dda81c70201564
Angor78 <angor78@gmail.com>
2021-12-23 16:06:54 +0300

    View_client_data

--- PythonSimpleServer.py
+++ PythonSimpleServer.py
@@ -2,3 +2,9 @@ import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('127.0.0.1', 2000))
+
+server.listen(4)
+print('Working....')
+client_socket, addres = server.accept()
+data = client_socket.recv(1024).decode('utf-8')
+print(data)
