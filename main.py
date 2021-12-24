593743a51b9ad31fb36c4944a1afb03d6c7c331c
Angor78 <angor78@gmail.com>
2021-12-24 13:18:32 +0300

    HDRS_404

--- main.py
+++ main.py
@@ -22,11 +22,15 @@ def start_my_server():
 
 def load_page_from_get_request(request_data):
     HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset: utf-8\r\n\r\n'
+    HDRS_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset: utf-8\r\n\r\n'
     path = request_data.split(' ')[1]
     response = ''
-    with open('veiw' + path, 'rb') as file:
-        response = file.read()
-    return HDRS.encode('utf-8') + response
+    try:
+        with open('veiw' + path, 'rb') as file:
+            response = file.read()
+        return HDRS.encode('utf-8') + response
+    except FileNotFoundError:
+        return(HDRS_404 + 'Sorry bro! No page...').encode('utf-8')
 
 
 if __name__ == '__main__':
